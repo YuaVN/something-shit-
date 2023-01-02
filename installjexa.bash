0e952f63d8df9e16ca3fc4c878b704743e1a9360
YuaVN <95994365+YuaVN@users.noreply.github.com>
2023-01-02 23:40:44 +0700

    Update installjexa.bash

--- installjexa.bash
+++ installjexa.bash
@@ -1,4 +1,10 @@
+########################
+# Edit the xstartup file
 
-apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php add-apt-repository ppa:redislabs/redis -y 
-# The command below is not needed if you are using Ubuntu 22.04 or higher. 
-curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash apt update apt -y install php8.1 php8.1-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+        cat > ~/.vnc/xstartup <<END
+#!/bin/shxrdb $HOME/.Xresources
+xsetroot -solid grey
+x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
+# x-window-manager &
+gnome-session &
+END
